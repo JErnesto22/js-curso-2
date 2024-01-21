4c9ce3f35606dcfc071b193a0ebe79c41524147c
Ernesto Ramirez <rmunoz.ernesto22@gmail.com>
2024-01-21 11:57:01 -0600

    DesafioDeProgramacionUsandoFunciones

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 
+let titulo = document.querySelector('.title');
+titulo.innerHTML = 'Hora del desafio';
+
+function consoleButton(){
+    alert('El boton fue clicado')
+}
+
+function promptButton(){
+    let ciudadDeBrasil = prompt('Ingresa el nombre de una ciudad de Brasil')
+    alert(`Estuve en ${ciudadDeBrasil} y me acordé de ti`);
+}
+
+function alertButton(){
+    alert('Yo amo Java Script <3')
+
+}
+
+function sumaButton(){
+    let numeroUno = parseInt(prompt('Ingresa un numero'));
+    let numeroDos = parseInt(prompt('Ingresa otro numero'));
+    let resultado = numeroDos + numeroUno;
+    alert(`El resultado es: ${resultado}`)
+}
